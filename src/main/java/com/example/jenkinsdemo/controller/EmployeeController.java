8bd8a623f594f7064f0fce62ddd545da78a4ae07
binaim <luwyzb@gmail.com>
2022-08-07 15:16:27 -0500

    first commit

---
+++ src/main/java/com/example/jenkinsdemo/controller/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.example.jenkinsdemo.controller;
+
+import com.example.jenkinsdemo.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @GetMapping("/{id}")
+    public Employee getEmployees(@PathVariable("id") String id){
+        Employee employee = new Employee("biniam","12",33);
+        return employee;
+    }
+}
